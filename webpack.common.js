1aa02a08ccc3508e38d16b33a4c9a4575777eebf
胡师寰 <hxfsc2@gmail.com>
2018-05-15 15:18:36 +0800

    react webpack initial

---
+++ webpack.common.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+module.exports = {
+    entry: {
+        app: path.resolve(__dirname,  "./src/index.js")
+    },
+    
+    output: {
+        filename: "[name].[hash].js",
+        //chunkFilename: "[name].[hash].js",
+        path: path.resolve(__dirname, "./dist")
+    },
+
+
+    resolve: {
+        extensions: ["*", ".js", ".jsx"],
+    },
+
+    module: {
+        rules: [
+            { test: /\.(js|jsx)$/, exclude: /node_modules/, loader: "babel-loader" }
+        ]
+    },
+
+    plugins: [
+        new CleanWebpackPlugin(["./dist"]),
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, "./src/index.html")
+        }),
+        // new webpack.optimize.CommonsChunkPlugin({
+        //     name: "common"
+        // })
+    ]
+
+}
