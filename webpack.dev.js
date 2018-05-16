fbbfd6c5b13d08b1c6cc4db9f0dc69c43c65b85c
胡师寰 <hxfsc2@gmail.com>
2018-05-16 16:07:48 +0800

    fix .editorconfig

--- webpack.dev.js
+++ webpack.dev.js
@@ -5,7 +5,7 @@ const common = require("./webpack.common");
 module.exports = merge(common, {
     mode: "development",
 	devtool: "inline-source-map",
-
+	
     plugins: [
         new webpack.NamedModulesPlugin(),
         new webpack.HotModuleReplacementPlugin()
